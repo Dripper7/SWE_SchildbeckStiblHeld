ad671dea7765fa911e26a9b11a14ca771695ad53
mar5chi <maschildbeck@gmail.com>
2023-01-14 22:59:39 +0100

    add file upload

---
+++ multiple_file_upload/main.py
@@ -0,0 +1,53 @@
+import os
+import urllib.request
+from app import app
+from flask import Flask, request, redirect, jsonify, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')  # GET, for browser, gui will be provided to user
+def upload_image():
+    return render_template('upload-multiple.html')
+
+@app.route('/multiple-files-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'files[]' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	
+	files = request.files.getlist('files[]')
+	
+	errors = {}
+	success = False
+	
+	for file in files:		
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			success = True
+		else:
+			errors[file.filename] = 'File type is not allowed'
+	
+	if success and errors:
+		errors['message'] = 'File(s) successfully uploaded'
+		resp = jsonify(errors)
+		resp.status_code = 206
+		return resp
+	if success:
+		resp = jsonify({'message' : 'Files successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify(errors)
+		resp.status_code = 400
+		return resp
+
+if __name__ == "__main__":
+    #app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
