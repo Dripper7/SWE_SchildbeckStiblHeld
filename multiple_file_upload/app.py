b082f8cbfe7df467b91e9f10f6f6cfe59a10e8e9
Stefan PC <stefan_held@hotmail.de>
2023-01-16 22:42:20 +0100

    frontend and download start

--- multiple_file_upload/app.py
+++ multiple_file_upload/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 UPLOAD_FOLDER = 'uploads'
+DOWNLOAD_FOLDER = 'downloads'
 
 app = Flask(__name__)
 #app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
--- multiple_file_upload/main.py
+++ multiple_file_upload/main.py
@@ -6,13 +6,16 @@ from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+#check File type 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+
+#upload image
 @app.route('/')  # GET, for browser, gui will be provided to user
 def upload_image():
     return render_template('upload-multiple.html')
 
+#multiple files upload 
 @app.route('/multiple-files-upload', methods=['POST'])
 def upload_file():
 	# check if the post request has the file part
@@ -48,6 +51,48 @@ def upload_file():
 		resp.status_code = 400
 		return resp
 
+#multiple files download 
+@app.route('/multiple-files-download', methods=['GET'])
+def download_file():
+	# check if the post request has the file part
+    if 'files[]' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    
+    files = request.files.getlist('files[]')
+    
+    errors = {}
+    success = False
+    
+    for file in files:		
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['DOWNLOAD_FOLDER'], filename))
+            success = True
+        else:
+            errors[file.filename] = 'File type is not allowed'
+    
+    if success and errors:
+        errors['message'] = 'File(s) successfully downloaded'
+        resp = jsonify(errors)
+        resp.status_code = 206
+        return resp
+    if success:
+        resp = jsonify({'message' : 'Files successfully downloaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 400
+        return resp
+	
+
+    
+    
+    
+
 if __name__ == "__main__":
     #app.run()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
