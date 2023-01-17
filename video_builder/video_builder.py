dd15053426a94306258c086c535b8e79bcbd428c
mar5chi <maschildbeck@gmail.com>
2023-01-17 19:04:40 +0100

    small change

--- video_builder/video_builder.py
+++ video_builder/video_builder.py
@@ -2,6 +2,7 @@ import subprocess
 
 
 def build():
+	fps, duration = 2, 5
 	subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "uploads/%02d.png","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "downloads/video.avi"])
 
 
