4d2a28fef32d110dd96b7af30d0a330c408a3d83
mar5chi <82018970+mar5chi@users.noreply.github.com>
2023-01-18 16:04:31 +0100

    Update video_builder.py

--- video_builder/video_builder.py
+++ video_builder/video_builder.py
@@ -3,9 +3,9 @@ import subprocess
 
 def build():
 	fps, duration = 2, 5
-	subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "uploads/%02d.png","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "downloads/video.avi"])
+	subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "uploads/%02d.png","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "downloads/video_new.avi"])
 
 
 if __name__ == "__main__":
 	build()
-
+	print("All well!")
